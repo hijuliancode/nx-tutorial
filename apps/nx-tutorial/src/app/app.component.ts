0fde3247b536ff07dc5ca2b4233d6d7f04cb4fab
hijuliancode <hijuliansosa@protonmail.com>
2020-08-27 10:04:21 -0500

    Display Todos, Conenct to API, Add Node Application, Configure Proxy

--- apps/nx-tutorial/src/app/app.component.ts
+++ apps/nx-tutorial/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+interface Todo {
+  title: string
+}
 
 @Component({
   selector: 'nx-tutorial-root',
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'nx-tutorial';
+  title: string = 'Todos'
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch()
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    })
+  }
+
 }
